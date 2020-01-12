c84c273746213b25323ed2e7ebd271d3ea311fda
rickynyairo <rickynyairo@gmail.com>
2020-01-12 17:44:41 +0300

    adds logging and request-id middleware

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/golang/protobuf v1.3.2
+	github.com/grpc-ecosystem/go-grpc-middleware v1.1.0
 	github.com/grpc-ecosystem/grpc-gateway v1.12.1
 	go.uber.org/zap v1.13.0
 	google.golang.org/genproto v0.0.0-20200108215221-bd8f9a0ef82f
--- pkg/protocol/rest/server.go
+++ pkg/protocol/rest/server.go
@@ -2,16 +2,17 @@ package rest
 
 import (
 	"context"
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
 
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	v1 "github.com/rickynyairo/go-todo-list/pkg/api/v1"
+	"github.com/rickynyairo/go-todo-list/pkg/logger"
+	"github.com/rickynyairo/go-todo-list/pkg/protocol/rest/middleware"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
-
-	"github.com/rickynyairo/go-todo-list/pkg/api/v1"
 )
 
 // RunServer runs HTTP/REST gateway
@@ -22,12 +23,14 @@ func RunServer(ctx context.Context, grpcPort, httpPort string) error {
 	mux := runtime.NewServeMux()
 	opts := []grpc.DialOption{grpc.WithInsecure()}
 	if err := v1.RegisterToDoServiceHandlerFromEndpoint(ctx, mux, "localhost:"+grpcPort, opts); err != nil {
-		log.Fatalf("failed to start HTTP gateway: %v", err)
+		logger.Log.Fatal("failed to start HTTP gateway", zap.String("reason", err.Error()))
 	}
 
 	srv := &http.Server{
-		Addr:    ":" + httpPort,
-		Handler: mux,
+		Addr: ":" + httpPort,
+		// add handler with middleware
+		Handler: middleware.AddRequestID(
+			middleware.AddLogger(logger.Log, mux)),
 	}
 
 	// graceful shutdown
@@ -44,6 +47,6 @@ func RunServer(ctx context.Context, grpcPort, httpPort string) error {
 		_ = srv.Shutdown(ctx)
 	}()
 
-	log.Println("starting HTTP/REST gateway...")
+	logger.Log.Info("starting HTTP/REST gateway...")
 	return srv.ListenAndServe()
 }
